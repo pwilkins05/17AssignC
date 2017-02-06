18302f570ed8220a2937e235fad8bf4e3e3d080b
OliverBourland <jeremyredthorn@gmail.com>
2017-02-06 15:54:49 -0800

    Fixed lowercase reroll

--- yahtzee.cpp
+++ yahtzee.cpp
@@ -17,7 +17,7 @@
 // TODO switch to using array for dice
 // TODO: add yahtzee bonus score
 // TODO: implement scoring functions (see switch statement, line 125)
-// TODO: make ask reroll accept lowercase letters
+// TODO: make ask reroll accept lowercase letters **FINISHED**
 // BUG: dice not holding correct values
 
 using namespace std;
@@ -215,6 +215,10 @@ bool askReroll(int n)
                 return true;
             case 'N':
                 return false;
+			case 'y':
+				return true;
+			case 'n':
+				return false;
             default:
                 cout << "Invalid response" << endl;
         }
