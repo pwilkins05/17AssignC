83ed72759afddef8673d00411ebe4735698d3509
OliverBourland <jeremyredthorn@gmail.com>
2017-02-08 15:27:10 -0800

    added name

--- yahtzee.cpp
+++ yahtzee.cpp
@@ -17,7 +17,7 @@
 // TODO switch to using array for dice  **Finished - Ryan**
 // TODO: add yahtzee bonus score
 // TODO: implement scoring functions (see switch statement, line 125)
-// TODO: make ask reroll accept lowercase letters **FINISHED**
+// TODO: make ask reroll accept lowercase letters **FINISHED - Oliver**
 // BUG: dice not holding correct values
 
 /*I've not learned C++ and was having trouble understanding the syntax for turning the scores into arrays,
