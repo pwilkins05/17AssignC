ee0213af03b58a89887fd1afd1e72a7eb8873fc3
OliverBourland <jeremyredthorn@gmail.com>
2017-02-08 15:34:03 -0800

    Oliver Fixed Garnick Bug Array

--- GarnickApp.cs
+++ GarnickApp.cs
@@ -7,8 +7,9 @@ namespace Application
 		{
 		//Array should contain 1 - 100
 		//#Bug: This does not contain 1 - 100
+		// Bug Fixed by Oliver
 		int[] Array = new int[100];
-		for (int i = 0, i < 100, i++)
+		for (int i = 1, i <= 100, i++)
 			Array[i] = i;
 
 		return Array;
