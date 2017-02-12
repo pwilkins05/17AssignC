9080f2a4cc3caf5e2184d6a2f9e57e14526e5889
Devin <devin.a.dubose@gmail.com>
2017-02-12 10:20:21 -0800

    Add determining odd/even number.

--- GarnickApp.cs
+++ GarnickApp.cs
@@ -1,37 +1,42 @@
 using System;
+
 namespace Application
 {
-	public class GarnickApp
-	{
-		public int[] returnArray()
-		{
-		//Array should contain 1 - 100
-		//#Bug: This does not contain 1 - 100
-		// Bug Fixed by Oliver
-		int[] Array = new int[100];
-		for (int i = 1, i <= 100, i++)
-			Array[i] = i;
+    public class GarnickApp
+    {
+        public int[] returnArray()
+        {
+            //Array should contain 1 - 100
+            //#Bug: This does not contain 1 - 100
+            // Bug Fixed by Oliver
+	    //Fixed a bug with commas rather than semi colons --Devin DuBose
+            int[] Array = new int[100];
+            for (int i = 1; i <= 100; i++)
+                Array[i] = i;
 
-		return Array;
-		}
+            return Array;
+        }
 
         //I didn't originally add enough lines of code to fullfill the requirements for the assignment, so here is more
         //This does not properly identify even or odd numbers
+
+	//Fixed the bug with determining even/odd, as well as some read/write line bugs. --Devin DuBose
         static void Main(string[] args)
         {
             int i;
-            Console.Write("Enter a Number : ");
+            Console.WriteLine("Enter a Number : ");
             i = int.Parse(Console.ReadLine());
-            if (i % 3 == 0)
+            if (i % 2 == 0)
             {
-                Console.Write("Entered Number is an Even Number");
-                Console.Read();
+                Console.WriteLine("Entered Number is an Even Number");
+
             }
             else
             {
-                Console.Write("Entered Number is an Odd Number");
-                Console.Read();
+                Console.WriteLine("Entered Number is an Odd Number");
             }
+
+            Console.ReadLine();
         }
 
     }
